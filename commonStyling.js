a1ffe8bc336c8e6ebdfaa964eaf631347a0e50a7
Sam Martin <sam.martin@7digital.com>
2019-05-05 20:09:50 +0100

    Android compatible

---
+++ commonStyling.js
@@ -0,0 +1,8 @@
+import {Platform} from 'react-native'
+export const primary = '#D36135'
+export const secondary = '#401F3E'
+export const light = '#E8E9EB'
+export const highlight = '#2274A5'
+export const dark = '#313638'
+export const headerFont = Platform.OS === 'ios' ? 'Helvetica' : 'sans-serif';
+export const paragraphFont = Platform.OS === 'ios' ? 'Georgia' : 'sans-serif';
