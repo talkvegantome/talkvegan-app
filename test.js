30d759a690b25e736cc72806369b139d24d3f706
Sam Martin <sam.martin@7digital.com>
2019-05-05 21:48:58 +0100

    Markdown Cleanup and added tests

--- test.js
+++ test.js
@@ -6,4 +6,10 @@ _.map(data, (menuItem, index) => {
   test("index.json: '" + menuItem.friendlyName + "' machine name has a value", () => {
       expect(menuItem.machineName).not.toBeNull()
   })
+  test("index.json: '" + menuItem.friendlyName + "' content has no single newlines (breaks MD)", () => {
+      let pattern = /([^\n\-\d])\n([^\n\-\d])/
+      //console.log(menuItem.rawContent.match(pattern))
+      expect(!menuItem.rawContent.match(pattern));
+
+  })
 });
