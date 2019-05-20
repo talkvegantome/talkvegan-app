00b706d48b7d6ec842c7c819ef2b56164f29a0ec
Sam Martin <sam.martin@7digital.com>
2019-05-20 21:32:48 +0100

    Fixed mock

--- src/settings/SettingScreen.test.js
+++ src/settings/SettingScreen.test.js
@@ -3,8 +3,11 @@ import SettingsScreen from './SettingsScreen.js';
 import renderer from 'react-test-renderer';
 
 import {MockStorage} from '../../mocks/MockStorage.js'
-
 let mockStorage = new MockStorage()
+
+import RNAmplitude from 'react-native-amplitude-analytics';
+jest.mock('react-native-amplitude-analytics');
+
 
 test('SettingsScreen Renders Correctly', () => {
   const tree = renderer.create(<SettingsScreen storage={mockStorage}/>).toJSON();
